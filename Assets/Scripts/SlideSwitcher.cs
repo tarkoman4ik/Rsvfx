f303490496b1d7a260d16a9338ab28a99d6ea66f
tarkoman4ik <130692562+tarkoman4ik@users.noreply.github.com>
2023-09-02 11:14:12 +0500

    Update

--- Assets/Scripts/SlideSwitcher.cs
+++ Assets/Scripts/SlideSwitcher.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SlideSwitcher : MonoBehaviour
 {
-    public float time = 10.0f,duration=1.0f,timeLeft=30.0f;
+    public float time = 3.0f,duration=1.0f,timeLeft=30.0f;
     public bool NextSceneUpdate = false;
+    public GameObject video1,video2;
+    private VideoPlayer videoPlayer1,videoPlayer2;
+    public float timeVideo = 83.0f;
+    private bool first = false, second = false;
     private void Start()
     {
+        videoPlayer1 = video1.GetComponent<VideoPlayer>();
+        videoPlayer2 = video2.GetComponent<VideoPlayer>();
+        videoPlayer1.Pause();
+        videoPlayer2.Pause();
         DOTween.Sequence()
             .AppendInterval(time)
             .Append(transform.DOMove(endValue: new Vector3(x: -38, y: 0.77f, z: 0), duration: duration))
@@ -19,30 +28,56 @@ public class SlideSwitcher : MonoBehaviour
             .Append(transform.DOMove(endValue: new Vector3(x: -114, y: 0.77f, z: 0), duration: duration))
             .AppendInterval(time)
             .Append(transform.DOMove(endValue: new Vector3(x: -152, y: 0.77f, z: 0), duration: duration))
-            .AppendInterval(time)
+            .AppendInterval(time+5.0f)
             .Append(transform.DOMove(endValue: new Vector3(x: -190, y: 0.77f, z: 0), duration: duration))
             .AppendInterval(time)
             .Append(transform.DOMove(endValue: new Vector3(x: -228, y: 0.77f, z: 0), duration: duration))
             .AppendInterval(time)
+            .Append(transform.DOMove(endValue: new Vector3(x: -266, y: 0.77f, z: 0), duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMove(endValue: new Vector3(x: -304, y: 0.77f, z: 0), duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMove(endValue: new Vector3(x: -342, y: 0.77f, z: 0), duration: duration))
+            .AppendInterval(time+87.0f)
+            .Append(transform.DOMove(endValue: new Vector3(x: 0, y: 0.77f, z: 0), duration: duration))
             .SetLoops(-1);
     }
     private void Update()
     {
-        if (NextSceneUpdate)
+        timeVideo-= Time.deltaTime;
+        if (timeVideo < 0 && first == false&&second==false)
         {
-            NextScene();
-            NextSceneUpdate = false;
+            timeVideo += 25.0f;
+            first = true;
+            videoPlayer1.Play();
         }
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
-            NextSceneUpdate = true;
+        if (timeVideo<0&&first==true&&second==false)
+        {
+            timeVideo += 85.0f;
+            first = false;
+            second = true;
+        }
+        if (timeVideo < 0 && second == true)
+        {
+            videoPlayer2.Play();
+            second = false;
+            timeVideo += 192.0f;
+        }
+        //if (NextSceneUpdate)
+        //{
+        //    NextScene();
+        //    NextSceneUpdate = false;
+        //}
+        //timeLeft -= Time.deltaTime;
+        //if (timeLeft < 0)
+        //    NextSceneUpdate = true;
     }
     public void NextScene()
     {
         var index = SceneManager.GetActiveScene().buildIndex;
-        if (index == 3)
+        if (index == 1)
             SceneManager.LoadScene(0);
         else
-            SceneManager.LoadScene(index+1);
+            SceneManager.LoadScene(index + 1);
     }
 }
---
+++ Assets/Scripts/SlideSwitcherVertical.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SlideSwitcherVertical : MonoBehaviour
+{
+    public float time = 20.0f, duration = 1.0f;
+    public GameObject video1, video2,video3;
+    private VideoPlayer videoPlayer1, videoPlayer2,videoPlayer3;
+
+    private void Start()
+    {
+        DOTween.Sequence()
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 36.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 71.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 106.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 141.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 176.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 211.2f, duration: duration))
+            .AppendInterval(time)
+            .Append(transform.DOMoveY(endValue: 1.2f,duration:duration))
+            .SetLoops(-1);
+    }
+}
